8c7e7cd25bacfe3bdad466779d7cc881304696ca
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-05-12 19:42:41 +0500

    Questions list from a file realized

--- app/src/main/java/com/avalitov/quizapp/MainActivity.kt
+++ app/src/main/java/com/avalitov/quizapp/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.avalitov.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+
+lateinit var btn_start : Button
+lateinit var et_name : AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_start = findViewById(R.id.btn_start)
+        et_name = findViewById(R.id.et_name)
 
+        btn_start.setOnClickListener {
+
+            if(et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
