3dd84a46caae76ab16edc57912138e5f15149c63
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-06-03 21:11:12 +0500

    Markup is ready

--- app/src/main/java/com/avalitov/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/avalitov/quizapp/QuizQuestionsActivity.kt
@@ -27,6 +27,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition : Int = 1
     private var mQuestionsList : ArrayList<Question>? = null
     private var mSelectedOptionPosition : Int = 0
+    private var mCorrectAnswers : Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -123,9 +124,13 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                     }
                 } else {    //if user has selected an option
                     val question = mQuestionsList?.get(mCurrentPosition - 1)
-                    if(question!!.correctAnswer != mSelectedOptionPosition) {
+
+                    if(question!!.correctAnswer != mSelectedOptionPosition) {   //if the answer is incorrect
                         setAnswerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {    // if the answer is correct
+                        mCorrectAnswers++
                     }
+
                     setAnswerView(question.correctAnswer, R.drawable.correct_option_border)
 
                     if(mCurrentPosition == mQuestionsList!!.size) {
