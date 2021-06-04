6fa5e208245b2afc456b36450bfcc1f332f08c1d
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-06-04 18:30:54 +0500

    Questions changed to dogs

--- app/src/main/java/com/avalitov/quizapp/Constants.kt
+++ app/src/main/java/com/avalitov/quizapp/Constants.kt
@@ -12,100 +12,100 @@ object Constants{
 
         // 1
         val que1 = Question(
-            1, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_argentina,
-            "Argentina", "Australia",
-            "Armenia", "Austria", 1
+            1, "Начнём с простого. Кто это?",
+            R.drawable.dog_akita,
+            "Акита-ину", "Чау-чау",
+            "Лабрадор", "Кавказская овчарка", 1
         )
 
         questionsList.add(que1)
 
         // 2
         val que2 = Question(
-            2, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_australia,
-            "Angola", "Austria",
-            "Australia", "Armenia", 3
+            2, "А это что за хороший мальчик?",
+            R.drawable.dog_samoed,
+            "Лайка", "Хаски",
+            "Самоед", "Пиренейская горная", 3
         )
 
         questionsList.add(que2)
 
         // 3
         val que3 = Question(
-            3, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_brazil,
-            "Belarus", "Belize",
-            "Brunei", "Brazil", 4
+            3, "Может чмокнуть в ушко либо откусить лицо:",
+            R.drawable.dog_bullterier,
+            "Бульмастиф", "Бультерьер",
+            "Бурбуль", "Английский бульдог", 2
         )
 
         questionsList.add(que3)
 
         // 4
         val que4 = Question(
-            4, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_belgium,
-            "Bahamas", "Belgium",
-            "Barbados", "Belize", 2
+            4, "Ладно, давайте посложнее. Кто это?",
+            R.drawable.dog_kurtzhaar,
+            "Курцхаар", "Далматинец",
+            "Шотландский сеттер", "Английский пойнтер", 1
         )
 
         questionsList.add(que4)
 
         // 5
         val que5 = Question(
-            5, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_fiji,
-            "Gabon", "France",
-            "Fiji", "Finland", 3
+            5, "А кто вырастет из этого малыша?",
+            R.drawable.dog_pitbool,
+            "Бигль", "Дог",
+            "Шарпей", "Питбуль", 4
         )
 
         questionsList.add(que5)
 
         // 6
         val que6 = Question(
-            6, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_germany,
-            "Germany", "Georgia",
-            "Greece", "none of these", 1
+            6, "Добавим немного экзотики. Кто тут у нас?",
+            R.drawable.dog_basengi,
+            "Басенджи", "Керн-терьер",
+            "Ка-де-бо", "Хоккайдо", 1
         )
 
         questionsList.add(que6)
 
         // 7
         val que7 = Question(
-            7, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_denmark,
-            "Dominica", "Egypt",
-            "Denmark", "Ethiopia", 3
+            7, "А это что за принцесса?",
+            R.drawable.dog_maltese,
+            "Ши-тцу", "Мальтийская болонка",
+            "Пудель", "Аркадий Укупник", 2
         )
 
         questionsList.add(que7)
 
         // 8
         val que8 = Question(
-            8, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_india,
-            "Ireland", "Iran",
-            "Hungary", "India", 4
+            8, "Это же этот, из рекламы!",
+            R.drawable.dog_zennenhund,
+            "Ньюфаундленд", "Фокстерьер",
+            "Цвергшнауцер", "Зенненхунд", 4
         )
 
         questionsList.add(que8)
 
         // 9
         val que9 = Question(
-            9, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_new_zealand,
-            "Australia", "New Zealand",
-            "Tuvalu", "United States of America", 2
+            9, "Понятно, что шпиц, а какой?",
+            R.drawable.dog_germanspitz,
+            "Японский", "Померанский",
+            "Немецкий", "Финский", 3
         )
 
         questionsList.add(que9)
 
         // 10
         val que10 = Question(
-            10, "What country does this flag belong to?",
-            R.drawable.ic_flag_of_kuwait,
-            "Kuwait", "Jordan",
-            "Sudan", "Palestine", 1
+            10, "Машина для убийств с тихих европейских улочек, кто это?",
+            R.drawable.dog_praha_krysarik,
+            "Кёльнская мышарица", "Пражский крысарик",
+            "Белградский хомякоед", "Ослинский выдрогрыз", 2
         )
 
         questionsList.add(que10)
